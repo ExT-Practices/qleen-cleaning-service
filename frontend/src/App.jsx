d99efdfc2ff9b10e9f279e39b17fbee71a5ac5ed
KALPESH PRAJAPATI <kp483368@gmail.com>
2026-07-17 14:33:35 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
 import Navbar from "./Components/Navbar";
 import Hero from "./Components/Hero";
+import AboutUs from "./Components/AboutUs";
 import HowItWorks from "./Components/Howworks";
 import CleaningServicesStack from "./Components/Plans";
+import Cards from "./Components/Cards";
+import Multiple from "./Components/Multiple-section";
+import ServicePriceList from "./Components/Service-price-list";
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />  
+    <div className="min-h-screen bg-white">
+      <Navbar />
       <Hero />
       <HowItWorks />
       <CleaningServicesStack />
+      <AboutUs />
+      <Cards />
+      <Multiple />
+      <ServicePriceList />
     </div>
-    
+
   );
 }
 
